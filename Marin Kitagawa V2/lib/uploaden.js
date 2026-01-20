6deab6f2d6b510b95d774950988a1b233996fb22
xvinn_voc <gadingkencana04@gmail.com>
2026-01-20 19:02:53 +0700

    Upload Marin Kitagawa V2/lib/uploaden.js

---
+++ Marin Kitagawa V2/lib/uploaden.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const axios = require('axios');
+const { fromBuffer } = require('file-type');
+
+async function uploaden(filePath) {
+  const buffer = fs.readFileSync(filePath);
+  const { ext, mime } = (await fromBuffer(buffer)) || {};
+  const fileName = Date.now() + "." + ext;
+
+  const folder = "uploads";
+  const responsej = await axios.post("https://pxpic.com/getSignedUrl", { folder, fileName }, {
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const { presignedUrl } = responsej.data;
+
+  await axios.put(presignedUrl, buffer, {
+    headers: {
+      "Content-Type": mime,
+    },
+  });
+
+  const cdnDomain = "https://files.fotoenhancer.com/uploads/";
+  const sourceFileUrl = cdnDomain + fileName;
+
+  return sourceFileUrl;
+}
+
+module.exports = { uploaden }
